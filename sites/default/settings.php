90bcce0673aa8c50cd1a7a2b6d92f025a3342d5a
Erica Cha Vang <laujmimna@Ericas-MacBook-Air.local>
2015-05-25 22:51:29 -0700

    add module again.

--- sites/default/settings.php
+++ sites/default/settings.php
@@ -212,7 +212,21 @@
  *   );
  * @endcode
  */
-$databases = array();
+$databases = array (
+  'default' =>
+  array (
+    'default' =>
+    array (
+      'database' => 'erica_cipher',
+      'username' => 'epicodus',
+      'password' => 'epicodus',
+      'host' => 'localhost',
+      'port' => '',
+      'driver' => 'mysql',
+      'prefix' => '',
+    ),
+  ),
+);
 
 /**
  * Access control for update.php script.
@@ -244,7 +258,7 @@ $update_free_access = FALSE;
  *   $drupal_hash_salt = file_get_contents('/home/example/salt.txt');
  *
  */
-$drupal_hash_salt = '';
+$drupal_hash_salt = 'nrnGHTWPKOQJzfVoF9jke8Tll8s03Ck02jEudq5X0dU';
 
 /**
  * Base URL (optional).
@@ -565,3 +579,7 @@ $conf['404_fast_html'] = '<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML+RDFa 1.0//EN"
  * Remove the leading hash signs to disable.
  */
 # $conf['allow_authorize_operations'] = FALSE;
+
+error_reporting(E_ALL);
+ini_set('display_errors', TRUE);
+ini_set('display_startup_errors', TRUE);
